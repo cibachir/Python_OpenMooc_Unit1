d52fbf65899f5fb2b5327a69c07492cd5252343f
cibachir <cibachir@hotmail.com>
2018-08-09 21:12:20 +0300

    creation of the file 'quoteProject.py'

---
+++ quoteProject.py
@@ -0,0 +1,14 @@
+
+
+quotes=["If one has good manners, one may attain the same level of merit as those who spend their nights in prayer", 
+		"The best among you is he who learns the Quran and teaches it","A person will be with those whom he loves (in the Hereafter)" ]
+# Display a randomly choosen quote 
+print(quotes[2])
+# Ask user to prompt "enter" for another citation and "b" to exit  
+user_answer= input("prompt 'enter' for another citation and 'B' to exit \n ")
+if user_answer= "B":
+ 	# exit program
+ 	pass 
+else:
+	#display anoter randomly choosen quote 
+	print(quotes[0])
