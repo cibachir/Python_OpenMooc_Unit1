cd5fde4c85fa1b944305426bd9d3e7fbc6cc9c64
cibachir <cibachir@hotmail.com>
2018-08-17 09:22:45 +0300

    define function get_random_item inside 'quoteProjest.py'

--- quoteProject.py
+++ quoteProject.py
@@ -2,13 +2,24 @@
 
 quotes=["If one has good manners, one may attain the same level of merit as those who spend their nights in prayer", 
 		"The best among you is he who learns the Quran and teaches it","A person will be with those whom he loves (in the Hereafter)" ]
+
+
+
+def get_random_item(myList):
+	# get random number
+	# get random element from myList
+	chosen_quote=myList[0]
+	# return the chosen element
+	return chosen_quote
+
+
 # Display a randomly choosen quote 
-print(quotes[2])
-# Ask user to prompt "enter" for another citation and "b" to exit  
-user_answer= input("prompt 'enter' for another citation and 'B' to exit \n ")
-if user_answer= "B":
- 	# exit program
+print(get_random_item(quotes))
+# Ask user to hit "enter" for another citation and "b" to exit  
+user_answer= input("hit 'enter' for another citation and 'B' to exit \n ")
+if user_answer== "B":
+ 	# exit program	
  	pass 
 else:
 	#display anoter randomly choosen quote 
-	print(quotes[0])
+	print(get_random_item(quotes))
